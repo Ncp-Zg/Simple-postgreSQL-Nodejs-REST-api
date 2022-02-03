154d1f13eea82878222f94c9ba709a64fb792697
Necip <ncptzgn@gmail.com>
2022-02-03 14:20:00 +0300

    updating and deleting student from database

--- src/student/controller.js
+++ src/student/controller.js
@@ -1,40 +1,83 @@
 const pool = require("../../config/db");
-const queries = require("./queries")
+const queries = require("./queries");
 
+const getStudents = (req, res) => {
+  pool.query(queries.getStudents, (error, results) => {
+    if (error) throw error;
+    res.status(200).json(results.rows);
+  });
+};
 
-const getStudents = (req,res) => {
-    pool.query(queries.getStudents,(error, results)=>{
-        if(error) throw error;
-        res.status(200).json(results.rows)
-    })
-}
+const getStudentById = (req, res) => {
+  const id = parseInt(req.params.id);
+  pool.query(queries.getStudentById, [id], (error, results) => {
+    if (error) throw error;
+    res.status(200).json(results.rows);
+  });
+};
 
-const getStudentById = (req,res) => {
-    const id =parseInt(req.params.id);
-    pool.query(queries.getStudentById, [id],(error, results)=>{
-        if(error) throw error;
-        res.status(200).json(results.rows)
-    })
-}
+const addStudent = (req, res) => {
+  const { name, email, age, dob } = req.body;
 
-const addStudent = (req,res) => {
-    const {name,email,age,dob} =req.body;
+  pool.query(queries.checkEmailExist, [email], (error, results) => {
+    if (results.rows.length) {
+      res.send("Email already exist.");
+    }else{
+        pool.query(
+      queries.addStudent,
+      [name, email, age, dob],
+      (error, results) => {
+        if (error) throw error;
+        res.status(201).send("Student created Successfully.");
+      }
+    );
+    }
+
     
-    pool.query(queries.checkEmailExist, [email], (error, results)=>{
-        if(results.rows.length){
-            res.send("Email already exist.");
-        }
+  });
+};
 
-        pool.query(queries.addStudent, [name,email,age,dob], (error,results) => {
-            if(error) throw error;
-            res.status(201).send("Student created Successfully.")
-
-        })
+const removeStudent = (req, res) => {
+  const id = parseInt(req.params.id);
+  pool.query(queries.getStudentById, [id], (error, results) => {
+      const noStudentFound=!results.rows.length;
+    if (noStudentFound) {
+      res.send("Student does not exist in the database, could not remove.");
+    }else{
+        pool.query(queries.removeStudent, [id],(err,results)=>{
+        if(err) throw err;
+        res.status(200).send("Student removed successfully.")
     })
+    }
+
+    
+  });
+};
+
+const updateStudent = (req, res) => {
+  const id = parseInt(req.params.id);
+  const {name} =req.body;
+
+  pool.query(queries.getStudentById, [id], (error, results) => {
+    const noStudentFound=!results.rows.length;
+  if (noStudentFound) {
+    res.send("Student does not exist in the database");
+  }else{
+      pool.query(queries.updateStudent, [name,id] , (error,results)=>{
+          if(error) throw error
+          res.status(200).send("Student updated successfully")
+      })
+  }
+
+
+  
 }
+  )}
 
 module.exports = {
-    getStudents,
-    getStudentById,
-    addStudent
-}
+  getStudents,
+  getStudentById,
+  addStudent,
+  removeStudent,
+  updateStudent
+};
--- src/student/routes.js
+++ src/student/routes.js
@@ -1,10 +1,12 @@
 const {Router} = require('express');
-const { getStudents,getStudentById,addStudent } = require('./controller');
+const { getStudents,getStudentById,addStudent,removeStudent,updateStudent } = require('./controller');
 
 const router = Router();
 
 router.get("/",getStudents)
 router.get("/:id",getStudentById)
 router.post("/",addStudent)
+router.put("/:id",updateStudent)
+router.delete("/:id",removeStudent)
 
 module.exports = router;
